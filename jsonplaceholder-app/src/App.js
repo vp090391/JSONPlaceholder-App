10bf66a6a7e2147b0fd2192a25de54ec7642bd1d
vp090391 <vp090391@gmail.com>
2020-03-19 22:53:34 +0300

    feat: implement post table

--- jsonplaceholder-app/src/App.js
+++ jsonplaceholder-app/src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            posts: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    posts: data,
+                    isLoading: false
+                });
+            })
+            .catch(err => {
+                console.log("Fetch for posts data failed");
+                this.setState({ isLoading: false });
+            });
+    }
+
+    render() {
+        return (
+            <table className='table'>
+                <caption>Post information</caption>
+
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>userId</th>
+                        <th>title</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <TableRow posts={this.state.posts}/>
+                </tbody>
+            </table>
+        )
+    }
 }
+
+let TableRow = (props) => {
+    return(
+        //TODO onclick on row
+        props.posts.map((post) =>
+        <tr key={post.id} className="tableRow">
+            <td>{post.id}</td>
+            <td>{post.userId}</td>
+            <td>{post.title}</td>
+        </tr>
+        )
+    )
+};
 
 export default App;
