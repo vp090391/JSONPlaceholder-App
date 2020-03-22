0c6534907b0fc099bc578f344d74559ba5a9b84e
vp090391 <vp090391@gmail.com>
2020-03-22 20:15:55 +0300

    docs: upload build files

---
+++ jsonplaceholder-app/build/precache-manifest.66800e28ed586900c1c284f5cc084a7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e9b9bbc8ab2bb4dd482a63177b37d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ef234cfa96845b6e944",
+    "url": "/static/css/main.df0d22f7.chunk.css"
+  },
+  {
+    "revision": "c8933b89f494faf459de",
+    "url": "/static/js/2.c7aa6642.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c7aa6642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef234cfa96845b6e944",
+    "url": "/static/js/main.8238115c.chunk.js"
+  },
+  {
+    "revision": "b2b9753f051b5d1d39f8",
+    "url": "/static/js/runtime-main.f7a84fbe.js"
+  }
+]);
